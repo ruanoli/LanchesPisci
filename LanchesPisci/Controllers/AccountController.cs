610895add19133d492af79095bf03630b26bfb8a
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-09-03 11:55:20 -0300

    Login

--- LanchesPisci/Controllers/AccountController.cs
+++ LanchesPisci/Controllers/AccountController.cs
@@ -1,11 +1,9 @@
 ﻿using LanchesPisci.ViewModels;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
-    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
@@ -18,7 +16,6 @@ namespace LanchesMac.Controllers
             _signInManager = signInManager;
         }
 
-        [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
             return View(new LoginViewModel()
@@ -37,9 +34,7 @@ namespace LanchesMac.Controllers
 
             if (user != null)
             {
-                var result = await _signInManager.PasswordSignInAsync(user,
-                    loginVM.Password, false, false);
-
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
                 if (result.Succeeded)
                 {
                     if (string.IsNullOrEmpty(loginVM.ReturnUrl))
@@ -59,25 +54,29 @@ namespace LanchesMac.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken] //Evita ataque de requisição entre sites (CSRF).
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(LoginViewModel registroVM)
         {
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser() { UserName = registroVM.UserName };
+                var user = new IdentityUser { UserName = registroVM.UserName };
                 var result = await _userManager.CreateAsync(user, registroVM.Password);
 
                 if (result.Succeeded)
                 {
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Login", "Account");
                 }
                 else
                 {
-                    this.ModelState.AddModelError("Registro", "Falha ao tentar registrar usuário.");
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
                 }
             }
-
             return View(registroVM);
         }
+
+
     }
 }
+
+
--- LanchesPisci/ViewModels/LoginViewModel.cs
+++ LanchesPisci/ViewModels/LoginViewModel.cs
@@ -6,13 +6,13 @@ namespace LanchesPisci.ViewModels
     {
         [Required(ErrorMessage = "Informe o nome do Usuário.")]
         [Display(Name ="Usuário")]
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
 
         [Required(ErrorMessage ="Informe a senha.")]
         [DataType(DataType.Password)]
         [Display(Name ="Senha")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
-        public string ReturnUrl { get; set; } //envia o usuário para a página que ele estava tentando acessar antes de logar.
+        public string? ReturnUrl { get; set; } //envia o usuário para a página que ele estava tentando acessar antes de logar.
     }
 }
